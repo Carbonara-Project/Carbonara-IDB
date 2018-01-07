04f646591f4f8ddd88066060e6941054476831fb
Andrea Fioraldi <andreafioraldi@gmail.com>
2018-01-07 16:57:45 +0100

    2nd pypi upload

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='carbonara-idb',
-    version='0.4.0_1',
+    version='0.4.0_2',
     description='Pure Python parser for IDA Pro databases (.idb files). Fork used in Guanciale.',
     author='Willi Ballenthin',
     author_email='willi.ballenthin@gmail.com',
